697d4e002714e86d0aabdd611218d2460ba590da
John O'Connor <oconnojb@gmail.com>
2018-06-05 20:31:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,9 +21,13 @@ function distanceTravelledInFeet(s, e) {
 
 function calculatesFarePrice(s, e) {
   let distance = distanceTravelledInFeet(s, e)
-  if (distance<=400) {
+  if (distance <= 400) {
     return 0
-  } else if (distance>400 && distance<2000) {
-    return (distance-400)*0.02
+  } else if (distance > 400 && distance <= 2000) {
+    return (distance - 400) * 0.02
+  } else if (distance > 2000 && distance <= 2500) {
+    return 25
+  } else {
+    return "cannot travel that far"
   }
 }
