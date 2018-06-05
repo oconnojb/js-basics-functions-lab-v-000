737db7379b1e87c9e9028ce1fcd22eeaa5f63967
John O'Connor <oconnojb@gmail.com>
2018-06-05 20:29:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,9 @@ function distanceTravelledInFeet(s, e) {
 
 function calculatesFarePrice(s, e) {
   let distance = distanceTravelledInFeet(s, e)
-  if (distance <=400) {
+  if (distance<=400) {
     return 0
+  } else if (distance>400 && distance<2000) {
+    return (distance-400)*0.02
   }
 }
